d49a05bfb71a15e652c19755c37fafa90b27ac8b
Sandra F <178869328+SandraF1@users.noreply.github.com>
2026-04-14 14:57:32 +1000

    After local dev run

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,15 +1,1 @@
-/* /// <reference types="vite/client" />
-
-declare global {
-  interface ImportMeta {
-    glob: (
-      pattern: string,
-      options?: any
-    ) => Record<string, () => Promise<any>>;
-  }
-}
-
-export {};
- */
-
 /// <reference types="vite/client" />
